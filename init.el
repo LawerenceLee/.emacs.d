1f0439e65dd6a27e1e46dfc84491e781d9e2bbd8
LawerenceLee <lucifitz.edward@gmail.com>
2018-01-16 05:31:09 -0800

    Cleaned and prettied up file

--- init.el
+++ init.el
@@ -1,10 +1,23 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General Emacs Configuration (no packages, just plain Emacs)
+
+;; Sets yes/no prompt to y/n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Package Repositories
+
 (require 'package)
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/")t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Custom Set
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -15,14 +28,28 @@
  '(custom-enabled-themes (quote (tango-dark)))
  '(package-selected-packages (quote (helm org-plus-contrib evil))))
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
-;; Packages Initalized at Start
-(require 'evil)
-(evil-mode 1)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Packages
+
 (require 'org)
 
-;; Sets yes/no prompt to y/n
-(fset 'yes-or-no-p 'y-or-n-p)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Packages Initalized at Start
+
+(require 'evil)
+(evil-mode 1)
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -30,12 +57,11 @@
 
 (require 'helm-config)
 
+;;(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) Does not work.
+
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 (global-set-key (kbd "C-x r b") 'helm-bookmarks)
-(global-set-key (kbd "C-x m") 'helm-M-x)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(global-set-key (kbd "C-x m") 'helm-M-x)  ;; Search all things that can be done w/M-x using Helm.
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)  ;; Shows a list of everything you copied before.
+(global-set-key (kbd "C-x C-f") 'helm-find-files) ;; Use C-j for TAB completion.
+
